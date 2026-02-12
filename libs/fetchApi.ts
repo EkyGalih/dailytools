ef8bcbac79966446a31e0555656927c8904e6b9d
EkyGalih <ekkygalih8@gmail.com>
2026-02-12 21:47:52 +0800

    fix::bug

--- libs/fetchApi.ts
+++ libs/fetchApi.ts
@@ -1,6 +1,8 @@
 export async function apiFetch(url: string, revalidate = 60) {
     return fetch(url, {
         headers: {
+            accept: "application/json",
+            "User-Agent": "mytools-Komik/1.0",
             "x-api-key": process.env.NEXT_PUBLIC_API_KEY!,
         },
 
